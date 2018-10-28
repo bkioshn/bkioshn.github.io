4cf0584e30eb2b11eb86bcfc57745aed088e0e15
bkioshn <35752733+bkioshn@users.noreply.github.com>
2018-10-28 13:43:39 +0700

    a

---
+++ bWebsite/indexSrcipt.js
@@ -0,0 +1,9 @@
+
+window.onload = function(){ 
+    document.getElementById('cosmetic').onclick = function() {
+        document.getElementById('cosPic').src = 'https://github.com/bkioshn/bkioshn.github.io/blob/master/other/CG1.png?raw=true';
+    };
+}; 
+    // your code 
+
+
