40ca295112e7bb34eace3835ec9d338afe385ed3
bkioshn <35752733+bkioshn@users.noreply.github.com>
2019-01-06 18:40:53 +0700

    commit

--- bWebsite/indexSrcipt.js
+++
@@ -1,9 +0,0 @@
-
-window.onload = function(){ 
-    document.getElementById('cosmetic').onclick = function() {
-        document.getElementById('cosPic').src = 'https://github.com/bkioshn/bkioshn.github.io/blob/master/other/CG1.png?raw=true';
-    };
-}; 
-    // your code 
-
-
